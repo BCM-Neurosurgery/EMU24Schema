e7ef3ea94df76a91bfe859227125a1c424a61f11
Paul J Steffan <paul.steffan@bcm.edu>
2024-04-22 13:33:52 -0500

    first schema

---
+++ EMU24/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='emu24',
+    version='1.0.0',
+    description='Schema for 24 hour EMU ingestion.',
+    author='Paul J Steffan',
+    author_email='paul.steffan@bcm.edu',
+    url='https://github.com/your_username/your_package',
+    packages=find_packages(),
+    
+)
