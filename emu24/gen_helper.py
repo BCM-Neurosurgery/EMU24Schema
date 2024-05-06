29d8b74ff87aebb914bf0e164dcfbe64c3923ad4
TomekFraczek <tfraczek@uw.edu>
2024-05-06 12:13:05 -0700

    Added username and password as args to gen helper

--- emu24/gen_helper.py
+++ emu24/gen_helper.py
@@ -1,6 +1,17 @@
 """Import * from this script to setup your interpreter for working with the dj schema"""
 
+import argparse
 import datajoint as dj
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--username', required=False)
+parser.add_argument('-p', '--password', required=False)
+args = parser.parse_args()
+print('Here')
+
+if args.username and args.password:
+    dj.config['database.user'] = args.username
+    dj.config['database.password'] = args.password
 
 dj.config['database.host'] = 'localhost'
 dj.config['database.port'] = 3306
@@ -19,5 +30,6 @@ dj.config['stores'] = {
 }
 dj.conn()
 
+print('Importing schema...')
 from emu24.EMU24 import *
 
