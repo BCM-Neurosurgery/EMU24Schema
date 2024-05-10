191b42b8262484e3eeb5f66a5af847ee71d9f1aa
TomekFraczek <tfraczek@uw.edu>
2024-05-10 10:07:09 -0700

    USe aggressive stitching by default

--- emu24/EMU24.py
+++ emu24/EMU24.py
@@ -360,7 +360,7 @@ class StitchedChunks(dj.Computed):
         for filetype, files in all_nsxs.items():
             if not files:
                 continue  # Skip filetypes that we don't have
-            stitched_nsx = StitchedNsXFile(files, start=start_ts, end=end_ts)
+            stitched_nsx = StitchedNsXFile(files, start=start_ts, end=end_ts, aggressive_concat=True)
             full_nsx_path = os.path.join(out_path, f'{task_name}.{filetype}')
             with open(full_nsx_path, 'wb') as f:
                 stitched_nsx.write(f)
