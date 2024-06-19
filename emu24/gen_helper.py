a28427095e1e8e383b8a37949fa43f3b0edf239e
TomekFraczek <tfraczek@uw.edu>
2024-06-19 11:36:51 -0700

    Reconfigure the gen_helper script to use dev, prod and deploy settings

--- emu24/gen_helper.py
+++ emu24/gen_helper.py
@@ -6,7 +6,7 @@ import datajoint as dj
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--username', required=False)
 parser.add_argument('-p', '--password', required=False)
-parser.add_argument('-t', '--test-mode', action='store_true')
+parser.add_argument('-m', '--mode', default="dev")
 args = parser.parse_args()
 print('Here')
 
@@ -17,7 +17,25 @@ if args.username and args.password:
 dj.config['database.host'] = 'localhost'
 dj.config['database.port'] = 3306
 
-if args.test_mode:
+# Run DataJoint in Development mode: for fast local testing and debugging
+if args.mode == 'dev':
+    dj.config['safemode'] = False
+    dj.config['stores'] = {
+        "Ext_Chunk": {
+            "protocol": "file",
+            "location": "data/datalake/data/emu/",
+            "stage": "data/datalake/data/emu/"
+        },
+        "Ext_Stitch": {
+            "protocol": "file",
+            "location": "/mnt/lake-database/new-stitched",
+            "stage": "/mnt/lake-database/new-stitched"
+        }
+    }
+
+# Run DataJoint in deploy mode: for initial deployment to the production server but not yet modifying real data
+elif args.mode == 'deploy':
+    dj.config['safemode'] = False
     dj.config['stores'] = {
         "Ext_Chunk": {
             "protocol": "file",
@@ -30,7 +48,9 @@ if args.test_mode:
             "stage": "/mnt/lake-database/test-stitched"
         }
     }
-else:
+
+# Run DataJoint in production mode: for true real running conditions
+elif args.mode == 'prod':
     dj.config['stores'] = {
         "Ext_Chunk": {
             "protocol": "file",
@@ -39,12 +59,16 @@ else:
         },
         "Ext_Stitch": {
             "protocol": "file",
-            "location": "/mnt/lake-database/stitched",
-            "stage": "/mnt/lake-database/stitched"
+            "location": "/mnt/lake-database/new-stitched",
+            "stage": "/mnt/lake-database/new-stitched"
         }
     }
+else:
+    raise KeyError("Unknown mode! Must be 'dev', 'prod' or 'deploy'")
+
 dj.conn()
 
 print('Importing schema...')
-from EMU24 import *
+from emu24.EMU24 import *
+
 
