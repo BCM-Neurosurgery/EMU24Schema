2ca94f7125c9bdfe720ea67bb9fe8fa2a2f76d49
TomekFraczek <tfraczek@uw.edu>
2024-07-22 15:42:02 -0700

    Fix command line arg inheretance

--- scripts/populate.py
+++ scripts/populate.py
@@ -1,4 +1,4 @@
-import argparse
+import datajoint
 
 from emu24.helper import *
 
@@ -20,9 +20,14 @@ def populate_all():
 def populate_patient(patient_name):
 
     query = Patient() & f"emu_id='{patient_name}'"
-    found = query.fetch1()
+    found = query.fetch()
 
-    patient_id = found[0]  # select somehow
+    if len(found) == 0:
+        raise KeyError(f'Did not find a patient with EMU id = {patient_name}')
+    elif len(found) > 1:
+        raise KeyError(f'Found multiple patients with this patient ID!')
+
+    patient_id = found[0][0]
     restriction = f'patient_id={patient_id}'
 
     print('Collecting matching NSP data chunks...')
@@ -41,17 +46,21 @@ def populate_patient(patient_name):
 if __name__ == '__main__':
 
     arg_parser = argparse.ArgumentParser(
+        parents=[login_parser],
         description="""
             Helper script for populating the index database
-            """
+            """,
+        add_help=False
     )
     arg_parser.add_argument(
-        'patient',
+        '--patient',
         type=str,
         help='Run the populate only for the patient with the given EMU identifier'
     )
 
     args = arg_parser.parse_args()
+    connect(args)
+    from emu24.schema import *
 
     if args.patient:
         populate_patient(args.patient)
