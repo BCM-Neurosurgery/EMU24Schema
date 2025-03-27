bbe01cf98be35f0957b178fbf88a9056e7a2fb55
TomekFraczek <tfraczek@uw.edu>
2025-03-27 18:24:29 -0500

    Delete comment source in TaskComments to prevent re-population

--- scripts/deduplicate_task_comments.py
+++ scripts/deduplicate_task_comments.py
@@ -53,7 +53,8 @@ def dedupe_comment(table, patient_id, task_id, nsp_id, preference, commit=False)
                   f'    {task_data_comments[0]["comment"]} \n'
                   f'    This timestamp: {to_delete["timestamp"]}     chosen timestamp: {chosen_ts}\n')
             if commit:
-                (table & to_delete_str).delete()
+                (table & to_delete_str).delete()         #
+                (TaskComments & to_delete_str).delete()  # Delete the source to avoid re-population
 
 
 if __name__ == '__main__':
